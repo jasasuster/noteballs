9a5653b8144da8330f53a10c0447cd932408f782
jasasuster <92645266+jasasuster@users.noreply.github.com>
2023-01-30 17:07:58 +0100

    first commit

---
+++ src/views/ViewNotes.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="notes">
+
+    <div class="card has-background-success-dark p-4 mb-5">
+      <div class="field">
+        <div class="control">
+          <textarea class="textarea" placeholder="Add a new note"></textarea>
+        </div>
+      </div>
+
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button class="button is-link has-background-success">Add new note</button>
+        </div>
+      </div>
+    </div>    
+
+    <div v-for="i in 3" class="card mb-4">
+      <div class="card-content">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, impedit rerum voluptas quisquam delectus nemo harum officiis inventore, enim libero veniam doloribus voluptatum eligendi aliquid laboriosam, dolores nam deleniti accusantium.
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
+  </div>
+</template>
